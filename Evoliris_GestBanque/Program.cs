375ec6652c56a14f0ff2dbcc2ce3f3fd838a5b26
Thierry Morre <thierry.morre@cognitic.be>
2024-09-18 09:52:06 +0200

    Implémenation des indexeurs

--- Evoliris_GestBanque/Program.cs
+++ Evoliris_GestBanque/Program.cs
@@ -1,31 +1,32 @@
-﻿using Models;
-
-namespace Evoliris_GestBanque
+﻿namespace Evoliris_GestBanque
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Banque banque = new Banque() { Nom = "EvolirisBanking" };
             Personne titulaire = new Personne() { Nom = "Norris", Prenom = "Chuck", DateNaiss = new DateTime(1953, 3, 10) };
 
             Courant courant = new Courant() { Numero = "001", LigneDeCredit = 500, Titulaire = titulaire };
+            banque.Ajouter(courant);
+
 
             Console.Write("Dépot de -100 : ");
-            courant.Depot(-100);
-            Console.WriteLine(courant.Solde);
+            banque["001"].Depot(-100);
+            Console.WriteLine(banque["001"].Solde);
 
 
             Console.Write("Dépot de 100 : ");
-            courant.Depot(100);
-            Console.WriteLine(courant.Solde);
+            banque["001"].Depot(100);
+            Console.WriteLine(banque["001"].Solde);
 
             Console.Write("Retrait de 200 : ");
-            courant.Retrait(200);
-            Console.WriteLine(courant.Solde);
+            banque["001"].Retrait(200);
+            Console.WriteLine(banque["001"].Solde);
 
             Console.Write("Retrait de 500 : ");
-            courant.Retrait(500);
-            Console.WriteLine(courant.Solde);
+            banque["001"].Retrait(500);
+            Console.WriteLine(banque["001"].Solde);
         }
     }
 }
---
+++ Models/Banque.cs
@@ -0,0 +1,24 @@
+﻿namespace Models
+{
+    public class Banque
+    {
+        private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+        public required string Nom { get; set; }
+
+        public Courant this[string numero]
+        {
+            get { return _comptes[numero]; }
+        }
+
+        public void Ajouter(Courant courant)
+        {
+            _comptes.Add(courant.Numero, courant);
+        }
+
+        public void Supprimer(string numero)
+        {
+            _comptes.Remove(numero);
+        }
+    }
+}
